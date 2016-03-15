0cbb85f6289a756a79b971ef911573abf701ee88
Mark Xue <mark@mxue.org>
2016-03-15 06:49:55 -0700

    successfully generated randomness

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lexer
 import qualified Data.Text as T
-import qualified Data.Map.Strict as M
+import qualified Data.Map.Lazy as M
 import qualified Data.Text.IO as TIO
 import System.Environment
 import System.Directory
+import System.Exit
 import Control.Exception
 import Control.Monad
+import Crypto.Random.DRBG
+import Control.Monad.CryptoRandom
+import Data.Int
+--import qualified Data.ByteString as B
+import qualified Data.ByteString as B
 
 main :: IO ()
 main = readArg `catch` handler
@@ -16,8 +23,20 @@ readArg :: IO()
 readArg = do
   (path:_) <- getArgs
   maps <-  M.filter (> 1) <$> readDir path
-  putStrLn $ show maps
-  putStrLn $ show $ M.size maps
+  --putStrLn $ show maps
+  putStrLn $ "Generated a dictionary of size: " ++ (show $ M.size maps)
+  r <- getRandom
+  putStrLn $ show r
+
+--genPassPhrase :: Int -> LexHgram
+
+getRandom :: IO Int64
+getRandom = do
+  g <- newGenIO :: IO CtrDRBG
+  case (crandom g :: Either GenError (Int64, CtrDRBG))of
+    Left err -> (error $ show err) >> exitFailure
+    Right (result, g2 ) -> return result
+
 
 
 recReadDir :: FilePath -> IO LexHgram
@@ -34,8 +53,6 @@ dirPaths :: FilePath -> IO [FilePath]
 --listDirectory unavailable, drop .. and .  
 dirPaths p = (map (p ++ ) )<$> (filter(\x -> head x /= '.')) <$> (dirContents p )
 
-  --(filter(\x -> head x /= '.')) <$> (dirContents p )
---(map (p ++ )) <$> filter (/= "..") <$> (filter(/= ".")) <$> (dirContents p )
 
 dirContents :: FilePath -> IO [FilePath]
 dirContents p = do
